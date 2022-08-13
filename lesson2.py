faa17a7a97fb7d3ab2d1968668a97a590ae2760c
I-am-Ihor <i94t@protonmail.com>
2022-08-13 13:27:55 +0300

    first commit

---
+++ lesson2.py
@@ -0,0 +1,45 @@
+# cost_bit = int(input('cost bitcoin = '))
+# per = int(input('percent = '))
+# total_cost = cost_bit * (per / 100) + cost_bit
+# print(total_cost)
+# print(cost_bit * (per / 100))
+
+
+# geo = int(input('Geo '))
+# alg = int(input('Alg '))
+# phy = int(input('Phy '))
+# Aver = (geo + alg + phy) / 3
+# if Aver < 4:
+#     print('Вам действительно нужно работать больше')
+# if 4 <= Aver <= 6:
+#     print('Тебе нужно больше работать')
+# if Aver >= 7:
+#     print('Хорошая работа!')
+
+
+
+
+# geo = int(input('Geo = '))
+# alg = int(input('Alg = '))
+# phy = int(input('Phy = '))
+
+# print('Average =', int(geo + alg + phy) / 3)
+
+
+
+# width = int(input('width = '))
+# height = int(input('height = '))
+# total_area = width * height
+# print(total_area)
+
+
+# cost_lt = int(input('Enter the cost: '))
+# percent = int(input('Enter the percent: '))
+# total_cost = cost_lt * (percent / 100) + cost_lt
+# print('Total cost LT =', total_cost)
+
+
+
+
+
+
