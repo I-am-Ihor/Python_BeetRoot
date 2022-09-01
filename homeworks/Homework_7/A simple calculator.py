2167b8dd8adb455ebbb3d1a24c8a5e0b47201c24
I-am-Ihor <i94t@protonmail.com>
2022-09-01 16:03:19 +0300

    homework_7_function

---
+++ homeworks/Homework_7/A simple calculator.py
@@ -0,0 +1,26 @@
+"""
+A simple calculator.
+Create a function called make_operation, which takes in a simple arithmetic operator
+as a first parameter (to keep things simple let it only be '+', '-' or '*') and an
+arbitrary number of arguments (only numbers) as the second parameter. Then return
+the sum or product of all the numbers in the arbitrary parameter.
+
+"""
+
+
+def make_operation(arithmetic_operator, *args):    # the function takes an algometric value and any number of parameters (numbers only)
+    if arithmetic_operator == '+':
+        return sum(num for num in args)
+    if arithmetic_operator == '*':
+        calculation = 1
+        for num in args:
+            calculation *= num
+        return calculation
+    if arithmetic_operator == '-':
+        calculation = args[0] * 2
+        for num in args:
+            calculation -= num
+        return calculation
+
+
+print(make_operation())
